d69598ed89fd633af5668684ce3b3874f3c860e8
oliveiraspedro <pedro.santosdeoliveira13@gmail.com>
2025-11-10 08:31:41 -0300

    build: configurado variaveis de ambiente para conn com db

--- todo-board/src/main/java/com/board/todo_board/TodoBoardApplication.java
+++ todo-board/src/main/java/com/board/todo_board/TodoBoardApplication.java
@@ -1,6 +1,7 @@
 package com.board.todo_board;
 
 import com.board.todo_board.ui.MainMenu;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,14 @@ public class TodoBoardApplication implements CommandLineRunner {
 	private MainMenu mainMenu;
 
 	public static void main(String[] args) {
+		// 1. Carrega as variáveis do arquivo .env
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> {
+			// 2. Define as variáveis de ambiente para o sistema
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
+		// 3. Inicia o Spring Boot
 		SpringApplication.run(TodoBoardApplication.class, args);
 	}
 
